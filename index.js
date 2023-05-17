0d28a953e4bbe096fb1be01f90bfe420a525c4cc
Qianyun Mu <msbluemu@gmail.com>
2023-05-17 16:36:52 +1000

    conplete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching (drivers, name){
+    return drivers.filter(function(driver){
+       return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+
+// function fuzzyMatch (drivers, name){
+//     return drivers.filter(function(driver){
+//         return 
+//     })
+// }
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.charAt(0) === name.charAt(0))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
